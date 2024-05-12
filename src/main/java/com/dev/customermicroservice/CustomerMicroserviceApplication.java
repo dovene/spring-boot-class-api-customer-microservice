b98c34df8f8769d0cbc6bdc286074dc9aa84c913
dovene <adovene@gmail.com>
2024-05-12 21:47:07 +0200

    enable actuator http trace

--- src/main/java/com/dev/customermicroservice/CustomerMicroserviceApplication.java
+++ src/main/java/com/dev/customermicroservice/CustomerMicroserviceApplication.java
@@ -1,7 +1,9 @@
 package com.dev.customermicroservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerMicroserviceApplication {
@@ -10,4 +12,9 @@ public class CustomerMicroserviceApplication {
 		SpringApplication.run(CustomerMicroserviceApplication.class, args);
 	}
 
+
+	@Bean
+	public InMemoryHttpExchangeRepository htttpTraceRepository() {
+		return new InMemoryHttpExchangeRepository();
+	}
 }
